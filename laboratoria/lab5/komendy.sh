8125336e4f8e8444a95507093bed45e2e7c13c5a
Jakub Muszynski <jmuszynski@bluesoft.com>
2023-02-07 01:22:22 +0100

    flask

--- laboratoria/lab5/komendy.sh
+++ laboratoria/lab5/komendy.sh
@@ -5,3 +5,5 @@ ansible -m setup localhost
 ansible -m debug -a 'msg={{ hostvars[inventory_hostname] }}' localhost
 
 ansible -m debug -a 'msg={{ hostvars[inventory_hostname] }}' localhost  -i inventory/dev/mojeinventory.yml
+
+ansible -m debug -a 'msg={{ przyklad_zmiennej_globalnej }}' localhost  -i inventory/dev/mojeinventory.ini  -e przyklad_zmiennej_globalnej='z_lini_komend'
