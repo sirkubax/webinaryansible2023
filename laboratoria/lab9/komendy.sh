83ab6fd5b5dd4bd83b471ca349f2a40ca6c3161b
Jakub Muszynski <jmuszynski@bluesoft.com>
2023-02-15 00:01:31 +0100

    update lab 9 ;

--- laboratoria/lab9/komendy.sh
+++ laboratoria/lab9/komendy.sh
@@ -4,7 +4,7 @@ cd /katalog/webinaryansible2023/playbooks/chatgpt_szablony
 
 ansible-playbook playbook_i_szablon_html/szablonuj.yml
 
-ansible-playbook playbook_generowane_dokumentu/szablonuj.yml
+ansible-playbook playbook_generowanie_dokumentu/szablonuj.yml
 
 ansible-playbook playbook_konfiguracja_uslug/szablonuj.yml
 
